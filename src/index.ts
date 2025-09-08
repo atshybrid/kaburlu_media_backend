323a9881eaa059ee91a0009b81e5a8a276c5bb0e
amoghnyatechsolutions <amoghnyatechsolutions@gmail.com>
2025-09-08 15:05:19 +0000

    Changes before Firebase Studio auto-run

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import app from "./app";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
